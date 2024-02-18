993f1dd04617bcd1bd3032afc54ed434da464e2a
Andy Waite <13400+andyw8@users.noreply.github.com>
2024-02-17 22:14:35 -0500

    Initial commit

---
+++ lib/tailwind_ui_view_components.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "tailwind_ui_view_components/version"
+
+module TailwindUiViewComponents
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "tailwind_ui_view_components"
+
+require "minitest/autorun"
